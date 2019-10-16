6bc30c5a3c29c9d5c1b7cb665ec0acd37a1cb6fb
Leandro <leandroperezb@gmail.com>
2019-10-16 20:05:45 -0300

    Hola mundo con contador implementado

--- app/src/main/java/com/leandro/holamundo/MainActivity.kt
+++ app/src/main/java/com/leandro/holamundo/MainActivity.kt
@@ -2,11 +2,25 @@ package com.leandro.holamundo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var contador: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textoContador = findViewById<TextView>(R.id.mostrarContador)
+        findViewById<Button>(R.id.contar).setOnClickListener {
+            contador++
+            textoContador.text = getString(R.string.mostrar_contador, contador)
+        }
+        findViewById<Button>(R.id.descontar).setOnClickListener {
+            if (contador > 0) contador--
+            textoContador.text = getString(R.string.mostrar_contador, contador)
+        }
     }
+
 }
